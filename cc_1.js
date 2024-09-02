cd6add442d5ab9dd5c99146947655e31d89bffc2
Bilal Patel <bilalpatel@outlook.com>
2024-09-01 23:20:25 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,16 @@ var  isActive = false;
 console.log(employeeName, typeof (employeeName))
 console.log(employeeID, typeof (employeeID))
 console.log(isActive, typeof (isActive))
+
+
+
+// Task 2: Compound Data Types
+
+let products = ["Keyboard", "Mouse", "Charger"]
+const productdetails = {
+    name: "Keyboard",
+     price: 30,
+      inStock = true}
+
+      console.log(products, productdetails);
+
